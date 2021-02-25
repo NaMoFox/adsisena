fc25a5981041150f8e30e0e9eab8bebcce902d37
NaMoFox <73260593+NaMoFox@users.noreply.github.com>
2021-02-24 19:21:38 -0500

    Add files via upload

---
+++ java_script/propuestap/js/app.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18){
+    greeting = 'Buenas Noches! ';
+} else if (hourNow > 12){
+    greeting = 'Buenas Tardes! ';
+} else if (hourNow > 0){
+    greeting = 'Buenos Dias! ';
+} else {
+    greeting = 'Bienvenidos! ';
+}
+
+document.write('<h3>' + greeting + '</h3>');
